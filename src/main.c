b908e8258d585150b191afd3b280f26c254a63b0
Johannes (jotrorox) Müller <mail@jotrorox.com>
2025-02-19 18:35:39 +0100

    Simple QOL improvement

--- src/main.c
+++ src/main.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <sys/stat.h>
 #include "assembler.h"
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <input.asm> <output binary>\n", argv[0]);
+    const char *output_name = "a.out";
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <input.asm> [output binary]\n", argv[0]);
         return 1;
     }
-    return assemble(argv[1], argv[2]);
+    
+    // Use provided output name if given, otherwise use default
+    const char *output_file = (argc == 3) ? argv[2] : output_name;
+    
+    int result = assemble(argv[1], output_file);
+    if (result == 0) {
+        // Make the output file executable (rwxr-xr-x)
+        chmod(output_file, 0755);
+    }
+    return result;
 }
