57944bc318949323d92ad9f26b41def1205f3d8a
Johannes (jotrorox) Müller <mail@jotrorox.com>
2025-04-23 00:01:52 +0200

    Improving the cli and adding a simple cli

--- README.md
+++ README.md
@@ -1,24 +1,80 @@
 # JASM Assembler
 
-ToDo
+A simple easy-to-use assembler for the JASM language written in Zig.
+It is meant to be fast, easy to use, and portable. For now it's only 
+for educational and recreational purposes. And it's not even close to 
+being complete. So don't expect it to be stable or feature-complete. 
+But it's a start. And it's fun to write.
+
+## Table of Contents
+
+- [Features](#features)
+- [Installation](#installation)
+  - [Building from Source](#building-from-source)
+  - [System Requirements](#system-requirements)
+- [Usage](#usage)
+- [Examples](#examples)
+  - [Hello World](#hello-world)
+- [Documentation](#documentation)
+- [License](#license)
+- [Contributing](#contributing)
+- [Author](#author)
 
 ## Features
 
-ToDo
+- Fast
+- Easy to use
+- Portable
+- Simple
+- Fun
+- Working ( not yet :) )
 
 ## Installation
 
+### Prerequisites
+
+- [Zig](https://ziglang.org/) (master branch or 1.15.0-dev.369+1a2ceb36c)
+- That's it, this is all you need to build the assembler from scratch.
+
 ### Building from Source
 
-ToDo
+Just run `zig build` in the project directory. This will build the assembler
+and place it in the `zig-out/bin` directory.
 
-### System Requirements
+```bash
+# Clone the repository
+$ git clone https://github.com/jotrorox/jasm.git
+$ cd jasm
 
-ToDo
+# Build the assembler
+$ zig build
+
+# Run the assembler
+$ ./zig-out/bin/jasm
+```
+
+Just run `zig build run` to build and run the assembler in one step.
+
+```bash
+# Clone the repository
+$ git clone https://github.com/jotrorox/jasm.git
+$ cd jasm
+
+# Build and run the assembler
+$ zig build run -- <arguments>
+```
+
+Or for a optimized build, run `zig build -Doptimize=ReleaseSafe`.
 
 ## Usage
 
-ToDo
+```bash
+jasm [options] [file]
+
+Options:
+  -h, --help      Display this help message
+  -V, --version   Display version information
+```
 
 ## Examples
 
@@ -36,8 +92,10 @@ This project is licensed under the MIT License - see the [LICENSE](LICENSE) file
 
 ## Contributing
 
-Contributions are welcome! Please feel free to submit a Pull Request.
+Contributions are welcome! Please feel free to submit a [Pull Request](https://github.com/jotrorox/jasm/pulls) or an [Issue](https://github.com/jotrorox/jasm/issues).
 
 ## Author
 
-Johannes (Jotrorox) Müller
+- **Johannes (jotrorox) Müller** - [jotrorox](https://github.com/jotrorox)
+
+See also the list of [contributors](https://github.com/jotrorox/jasm/contributors) who participated in this project.
--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,8 @@ const ansi = @import("ansi_styles.zig");
 const name = "jasm";
 const version = "0.2.0";
 const copyright = "Copyright (c) 2025 Johannes (jotrorox) Müller";
+const author = "Johannes (jotrorox) Müller";
+const license = @embedFile("../LICENSE");
 
 pub fn main() !void {
     // Combine styles used frequently for brevity
@@ -56,14 +58,48 @@ pub fn main() !void {
         std.debug.print("{s}Error:{s} {s}No input file provided.{s}\n", .{
             style_error, reset, style_err_msg, reset,
         });
-        std.debug.print("Run '{s} --help' for usage information.\n", .{name});
+        std.debug.print("Run '{s}{s}{s} --help' for usage information.\n", .{
+            style_prog, name, reset,
+        });
         std.process.exit(1);
     }
 
-    // --- Placeholder for successful execution ---
-    const input_file = cli_args.positional.items[0];
-    std.debug.print("Processing file: {s}{s}{s}\n", .{
-        style_file, input_file, reset,
-    });
-    std.debug.print("Processing complete.\n", .{});
+    if (cli_args.positional.items.len > 1) {
+        std.debug.print("{s}Error:{s} {s}Too many input files provided. Expected 1.{s}\n", .{
+            style_error, reset, style_err_msg, reset,
+        });
+        std.process.exit(1);
+    }
+
+    const input_filename = cli_args.positional.items[0];
+
+    // --- Read file and count lines ---
+    const file = std.fs.cwd().openFile(input_filename, .{}) catch |err| {
+        std.debug.print("{s}Error:{s} {s}Could not open file '{s}{s}{s}': {any}{s}\n", .{
+            style_error, // 1st {s}
+            reset, // 2nd {s}
+            style_err_msg, // 3rd {s} (For "Could not open...")
+            style_file, // 4th {s} (Style for filename)
+            input_filename, // 5th {s} (The filename itself)
+            reset ++ style_err_msg, // 6th {s} (Reset style, then error style for ':')
+            err, // {any} (The actual error value)
+            reset, // 7th {s} (Final reset)
+        });
+        std.process.exit(1);
+    };
+    defer file.close();
+
+    var line_count: usize = 0;
+    var buf_reader = std.io.bufferedReader(file.reader());
+    const reader = buf_reader.reader();
+    const buffer_size = 4096; // Adjust buffer size as needed
+    var buffer: [buffer_size]u8 = undefined;
+
+    while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+        _ = line; // We only care about the count, not the content
+        line_count += 1;
+    }
+
+    // Print the line count as the output just for testing
+    std.debug.print("{d}\n", .{line_count});
 }
